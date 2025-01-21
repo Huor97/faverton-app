885e2c8849a553e5c59f1417eb65ee66c4843bbe
Huor97 <huor97ka@gmail.com>
2025-01-21 23:50:45 +0100

    configuration

--- faverton-nuxt3/eslint.config.mjs
+++ faverton-nuxt3/eslint.config.mjs
@@ -1,28 +1,38 @@
 // @ts-check
-// import withNuxt from './.nuxt/eslint.config.mjs';
-
-// export default withNuxt(
-//   {
-//     rules: {
-//       '@stylistic/semi': [`error`, `always`],
-//       '@stylistic/quotes': [`error`, `backtick`],
-//     },
-//   },
-// );
-
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt({
-  rules: {
-    '@typescript-eslint/no-unused-vars': [`error`, { argsIgnorePattern: `^_` }],
-    'import/no-unresolved': `off`,
-    'import/named': `off`,
-  },
-  settings: {
-    'import/resolver': {
-      typescript: true,
-      node: true,
+export default withNuxt(
+  {
+    rules: {
+      '@stylistic/semi': [`error`, `always`],
+      '@stylistic/quotes': [`error`, `backtick`],
+      'import/no-unresolved': `off`,
+      'import/named': `off`,
     },
   },
-});
+  {
+    files: [`**/*.spec.ts`],
+    rules: {
+      '@typescript-eslint/ban-ts-comment': `off`,
+      '@typescript-eslint/no-explicit-any': `off`,
+    },
+  },
+);
+
+// @ts-check
+// import withNuxt from './.nuxt/eslint.config.mjs';
+
+// export default withNuxt({
+//   rules: {
+//     '@stylistic/semi': [`error`, `always`],
+//     // '@typescript-eslint/no-unused-vars': [`error`, { argsIgnorePattern: `^_` }],
+//     'import/no-unresolved': `off`,
+//     'import/named': `off`,
+//   },
+//   settings: {
+//     'import/resolver': {
+//       typescript: true,
+//       node: true,
+//     },
+//   },
+// });
