ab072af45a6418f9ac27aa789f450a84854872f8
Rouh <huor97ka@gmail.com>
2025-05-14 04:03:16 +0200

    corrected chart

--- faverton-nuxt3/app/components/faverton/card/FavertonCard.vue
+++ faverton-nuxt3/app/components/faverton/card/FavertonCard.vue
@@ -8,7 +8,7 @@ const toggleMenu = () => {
 <template>
   <div
     class="fixed z-[9000] bottom-0 md:h-[90vh] md:w-[33vw] md:top-10 md:left-3 overflow-auto"
-    :class="isOpen? 'h-[50vh]' : 'h-[100vh]'"
+    :class="isOpen? 'h-[50vh]' : 'h-[100%]'"
     style="background: linear-gradient(to bottom, rgba(149, 215, 183, 0.8), #E8FFD7);"
   >
     <UButton
