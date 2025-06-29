281b3d6c619c2677d300cc8dfca630ca989dfb65
Huor97 <hour97ka@gmail.com>
2025-06-29 04:18:50 +0200

    delete buton for mobile

--- faverton-nuxt3/app/components/faverton/card/FavertonCard.vue
+++ faverton-nuxt3/app/components/faverton/card/FavertonCard.vue
@@ -1,30 +1,11 @@
 <script setup lang="ts">
-const isOpen = ref(false);
-const toggleMenu = () => {
-  isOpen.value = !isOpen.value;
-};
+
 </script>
 
 <template>
   <div
-    :class="isOpen? 'h-[50vh]' : 'h-[100%]'"
     class="rounded-lg overflow-hidden shadow-inner mx-5 my-10 p-5 bg-gradient-to-br from-blue-50 to-green-50 border border-blue-200"
   >
-    <div
-      class="md:hidden rounded-b-lg flex justify-center p-2"
-    >
-      <UButton
-        class="transition-transform duration-300 ease-in-out"
-        :class="{ 'rotate-180': isOpen }"
-        @click="toggleMenu"
-      >
-        <UIcon
-          name="i-tabler-chevron-up"
-          class="text-lg"
-        />
-      </UButton>
-    </div>
-
     <slot />
   </div>
 </template>
